370270bf6583f8801fbb09877bf5a2820ee5dbed
ThePalet <ankqji@gmail.com>
2026-07-19 16:18:20 +0100

    Localize DeepSpace\src\ice\game\ModeDifficulty.kt

--- src/ice/game/ModeDifficulty.kt
+++ src/ice/game/ModeDifficulty.kt
@@ -13,6 +13,16 @@ enum class ModeDifficulty(var color: Color) :Localizable {
           this.localizedName = "神赐"
           description = "{SICK=2}圣水淅沥,与神同行,乐园就在此处..."
         }
+
+        en {
+          this.localizedName = "Divine Grace"
+          description = "{SICK=2}Holy water flows gently. Walk beside the Divine, and Paradise lies just ahead..."
+        }
+
+        uk_UA {
+          this.localizedName = "Божественна благодать"
+          description = "{SICK=2}Священна вода тихо струменіє. Ідіть поруч із Божественним — і Рай буде зовсім поруч..."
+        }
       }
     }
   },
@@ -20,8 +30,18 @@ enum class ModeDifficulty(var color: Color) :Localizable {
     init {
       localization {
         zh_CN {
-          this.localizedName = "洗礼"
-          description = "{HANG=2;2}福祸未分,命途难测,{ENDHANG}{JUMP}神谕者缄口不言"
+            this.localizedName = "洗礼"
+            description = "{HANG=2;2}福祸未分,命途难测,{ENDHANG}{JUMP}神谕者缄口不言"
+        }
+
+        en {
+          this.localizedName = "Baptism"
+          description = "{HANG=2;2}Blessing and calamity remain entwined; fate is unknowable.{ENDHANG}{JUMP}The Oracle remains silent."
+        }
+
+        uk_UA {
+          this.localizedName = "Хрещення"
+          description = "{HANG=2;2}Благословення й лихо ще не розділені, а доля залишається непередбачуваною.{ENDHANG}{JUMP}Оракул зберігає мовчання."
         }
       }
     }
@@ -30,8 +50,18 @@ enum class ModeDifficulty(var color: Color) :Localizable {
     init {
       localization {
         zh_CN {
-          this.localizedName = "棘罪"
-          description = "{SHAKE}{SPEED=0.4}圣光暗淡,神像蒙尘,亵渎者又将何去何从?"
+            this.localizedName = "棘罪"
+            description = "{SHAKE}{SPEED=0.4}圣光暗淡，神像蒙尘，亵渎者又将何去何从？"
+        }
+
+        en {
+            this.localizedName = "Crown of Sin"
+            description = "{SHAKE}{SPEED=0.4}The holy light grows dim, the sacred idol gathers dust. Where, then, shall the blasphemer find refuge?"
+        }
+
+        uk_UA {
+            this.localizedName = "Терновий гріх"
+            description = "{SHAKE}{SPEED=0.4}Святе світло згасає, священна статуя вкривається пилом. Куди ж тепер подасться блюзнір?"
         }
       }
     }
