17ee6f104588c405a7b86efbaa3df274859a02a7
ThePalet <ankqji@gmail.com>
2026-07-21 11:18:02 +0100

    Localized DeepSpace\src\ice\world\meta\IceStatCats.kt

--- src/ice/world/meta/IceStatCats.kt
+++ src/ice/world/meta/IceStatCats.kt
@@ -6,23 +6,83 @@ import universecore.util.handler.FieldHandler
 
 object IceStatCats {
   val 结构 = create("structure") {
-    localization { zh_CN { this.localizedName = "结构" } }
+    localization {
+        zh_CN {
+          this.localizedName = "结构"
+        }
+        en {
+          this.localizedName = "Structure"
+        }
+        uk_UA {
+          this.localizedName = "Структура"
+        }
+      }
   }
   val 流体传输 = create("liquidTransport") {
-    localization { zh_CN { this.localizedName = "流体传输" } }
+    localization {
+        zh_CN {
+          this.localizedName = "流体传输"
+        }
+        en {
+          this.localizedName = "Fluid Transport"
+        }
+        uk_UA {
+          this.localizedName = "Транспортування рідин"
+        }
+      }
   }
   val 其他 = create("other") {
-    localization { zh_CN { this.localizedName = "其他" } }
+    localization {
+        zh_CN {
+          this.localizedName = "其他"
+        }
+        en {
+          this.localizedName = "Other"
+        }
+        uk_UA {
+          this.localizedName = "Інше"
+        }
+      }
   }
 
   val neutron = create("neutron", 3) {
-    localization { zh_CN { this.localizedName = "中子能" } }
+    localization {
+        zh_CN {
+          this.localizedName = "中子能"
+        }
+        en {
+          this.localizedName = "Neutron Energy"
+        }
+        uk_UA {
+          this.localizedName = "Нейтронна енергія"
+        }
+      }
   }
   val matrix = create("matrix", 5) {
-    localization { zh_CN { this.localizedName = "矩阵" } }
+    localization {
+        zh_CN {
+          this.localizedName = "矩阵"
+        }
+        en {
+          this.localizedName = "Matrix"
+        }
+        uk_UA {
+          this.localizedName = "Матриця"
+        }
+      }
   }
   val heat = create("heat", 6) {
-    localization { zh_CN { this.localizedName = "热能" } }
+    localization {
+        zh_CN {
+          this.localizedName = "热能"
+        }
+        en {
+          this.localizedName = "Thermal Energy"
+        }
+        uk_UA {
+          this.localizedName = "Теплова енергія"
+        }
+      }
   }
   val reaction = create("reaction")
 
